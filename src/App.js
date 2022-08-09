7d8cb0612b2997a6a0b50908d399620794b338ee
nabin <nkbhaie2017@gmail.com>
2022-08-09 10:56:37 -0600

    add a route for finding perry dog

--- src/App.js
+++ src/App.js
@@ -1,27 +1,73 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import VendingMachine from "./VendingMachine";
-import Sardines from "./Sardines";
-import Chips from "./Chips";
-import Soda from "./Soda";
+// import VendingMachine from "./VendingMachine";
+// import Soda from "./Soda";
+import HomePage from "./HomePage";
+import Perry from "./Perry";
 
 function App() {
+
+    // preload App with default props
+    // App.defaultProps = {
+
+    /*
+    const defaultProps = {
+        dogs: [
+          {
+            name: "Whiskey",
+            age: 5,
+            src: whiskey,
+            facts: [
+              "Whiskey loves eating popcorn.",
+              "Whiskey is a terrible guard dog.",
+              "Whiskey wants to cuddle with you!"
+            ]
+          },
+          {
+            name: "Duke",
+            age: 3,
+            src: duke,
+            facts: [
+              "Duke believes that ball is life.",
+              "Duke likes snow.",
+              "Duke enjoys pawing other dogs."
+            ]
+          },
+          {
+            name: "Perry",
+            age: 4,
+            src: perry,
+            facts: [
+              "Perry loves all humans.",
+              "Perry demolishes all snacks.",
+              "Perry hates the rain."
+            ]
+          },
+          {
+            name: "Tubby",
+            age: 4,
+            src: tubby,
+            facts: [
+              "Tubby is really stupid.",
+              "Tubby does not like walks.",
+              "Angelina used to hate Tubby, but claims not to anymore."
+            ]
+          }
+        ]
+      }
+      */
+    // above is the prealoaded props
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact>
-          <VendingMachine />
+        <Route path="/dogs" exact>
+          <HomePage />
         </Route>
-        <Route path="/soda" exact>
-          <Soda />
+        <Route exact path="/dogs/perry">
+            <Perry />
         </Route>
-        <Route path="/chips" exact>
-          <Chips />
-        </Route>
-        <Route path="/sardines" exact>
-          <Sardines />
-        </Route>
+
       </Switch>
     </BrowserRouter>
   );
